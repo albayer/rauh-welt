45115a6cf50e59c2b0b2726ffdbced9e7ff19e64
albayer <alperenbayer@gmail.com>
2025-05-19 02:08:40 +0300

    add kategoriler
    güncelle butonu çalışmıyor!!!

--- admin/kategoriler.php
+++ admin/kategoriler.php
@@ -1,40 +1,180 @@
-<?php require_once('header.php'); ?>
+<?php require_once('header.php');
+if (isset($_GET['turDeleteID'])) {
+    $id = $_GET['turDeleteID'];
+    $turSil = $db->prepare('delete from kategoriler where id=?');
+    $turSil->execute(array($id));
+    if ($turSil->rowCount()) {
+        echo '<script>alert("Kayıt Silindi")</script><meta http-equiv="refresh" content="1; url=kategoriler.php">';
+    } else {
+        echo '<script>alert("Hata oluştu")</script><meta http-equiv="refresh" content="1; url=kategoriler.php">';
+    }
+} else if (isset($_GET['turUpdateID'])) {
+    $id = $_GET['turUpdateID'];
+    $turUpdate = $db->prepare('select * from kategoriler where id=?');
+    $turUpdate->execute(array($id));
+    $turUpdateFetch = $turUpdate->fetch();
+    echo '  <script>
+                document.addEventListener("DOMContentLoaded", function () {
+                var myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
+                myModal.show();
+                });
+            </script>';
+}
+?>
 <div class="row">
     <div class="col-md-6">
         <h3>Kategoriler</h3>
     </div>
     <div class="col-md-6 text-end">
-        <button class="btn btn-primary">Tür Ekle</button>
+        <!-- Button trigger modal -->
+        <div class="text-end">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                Tür Ekle
+            </button>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Yeni Tür Ekle</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form method="post" enctype="multipart/form-data">
+                            <input type="text" name="tur" placeholder="Tür Adını Girin" class="form-control" required>
+                            <select name="durum" class="form-control my-3" required>
+                                <option value="">Seçiniz</option>
+                                <option value="Yayında">Yayında</option>
+                                <option value="Taslak">Taslak</option>
+                            </select>
+                            <!-- CK Editor Start -->
+                            <textarea name="aciklama" id="aciklama" placeholder="Tür Açıklaması" class="form-control" required></textarea>
+                            <!-- CK Editor End -->
+                            <input type="file" name="gorsel" class="form-control my-3" required>
+                            <input type="submit" value="Kaydet" class="btn btn-success w-100" name="kaydet">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="col-12">
-
         <table class="table table-stripped">
             <thead>
                 <tr>
+                    <th>Görsel</th>
                     <th>Motosiklet Türü</th>
-                    <th>Açıklama</th>
                     <th>Durum</th>
-                    <th>Tarih</th>
+                    <th>Açıklama</th>
                     <th>Düzenle</th>
                     <th>Sil</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
+                <?php
+                $turEkleList = $db->prepare('select * from kategoriler where durum="Yayında" order by id desc');
+                $turEkleList->execute();
+                if ($turEkleList->rowCount()) {
+                    foreach ($turEkleList as $turEkleListSatir) {
+                ?>
+                        <tr>
+                            <td style="width: 100px;"><img src="<?php echo $turEkleListSatir['gorsel']; ?>" class="w-100"></td>
+                            <td><?php echo $turEkleListSatir['tur']; ?></td>
+                            <td><?php echo $turEkleListSatir['durum']; ?></td>
+                            <td><?php echo $turEkleListSatir['aciklama']; ?></td>
+                            <td><a href="kategoriler.php?turUpdateID=<?php echo $turEkleListSatir['id'] ?>"><button class="btn btn-warning">Düzenle</button></a></td>
+                            <td><a href="kategoriler.php?turDeleteID=<?php echo $turEkleListSatir['id'] ?>"><button class="btn btn-danger">Sil</button></a></td>
+                        </tr>
+                <?php
+                    }
+                }
+                ?>
             </tbody>
         </table>
-        <form method="get">
-            <input type="text" name="katTur" placeholder="Kategori Türü Girin" class="form-control">
-            <input type="text" name="katAciklama" placeholder="Açıklama Girin" class="form-control my-2">
-            <input type="submit" value="Kaydet" class="btn btn-success w-100">
-        </form>
     </div>
 </div>
+<!-- Tür ekle kayıt modülü Start -->
+<?php
+if (isset($_POST['kaydet'])) {
+
+    $gorsel = '../assets/img/' . $_FILES['gorsel']['name'];
+
+    if (move_uploaded_file($_FILES['gorsel']['tmp_name'], $gorsel)) {
+        $turEkle = $db->prepare('insert into kategoriler (tur,durum,aciklama,gorsel) values (?,?,?,?)');
+        $turEkle->execute(array($_POST['tur'], $_POST['durum'], $_POST['aciklama'], $gorsel));
+        if ($turEkle->rowCount()) {
+            echo '<script>alert("Yeni Tür Eklendi.")</script><meta http-equiv="refresh" content="0; url=kategoriler.php">';
+        } else {
+            echo '<script>alert("Hata oluştu")</script><meta http-equiv="refresh" content="0; url=kategoriler.php">';
+        }
+    }
+}
+?>
+<!-- Tür ekle kayıt modülü End -->
+<!-- Update Modal Start -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="false">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><?php echo $turUpdateFetch['tur']; ?></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="post">
+                    <input type="text" name="turUp" value="<?php echo $turUpdateFetch['tur']; ?>" class="form-control">
+
+                    <select name="durumUp" class="form-control my-2">
+                        <option value="<?php echo $turUpdateFetch['durum']; ?>"><?php echo $turUpdateFetch['durum']; ?></option>
+                        <option value="Yayında">Yayında</option>
+                        <option value="Taslak">Taslak</option>
+                    </select>
+                    <textarea name="aciklamaUp" rows="4" class="form-control my-2"><?php echo $turUpdateFetch['aciklama']; ?></textarea>
+                    <div style="display: flex; column-gap:5px;">
+                        <label for=""><b>Kullanılan Görsel:</b></label>
+                        <img src="<?php echo $turUpdateFetch['gorsel'] ?>" alt="" style="max-width: 100px;">
+                    </div>
+                    <input type="file" name="gorselUp" class="form-control my-2">
+                    <input type="submit" value="Güncelle" class="btn btn-success w-100" name="kaydetUp">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Update Modal End -->
+<!-- Update Modal Module Start -->
+
+<?php
+
+if (isset($_POST['kaydetUp'])) {
+$gorselUp = '../assets/img/' . $_FILES['gorselUp']['name'];
+    if (move_uploaded_file($_FILES['gorselUp']['tmp_name'], $gorselUp)) {
+        $guncelle = $db->prepare('update kategoriler set tur=?, durum=?, aciklama=?, gorsel=? where id=?');
+        $guncelle->execute(array($_POST['turUp'], $_POST['durumUp'], $_POST['aciklamaUp'], $gorselUp, $id));
+        if ($guncelle->rowCount()) {
+            echo '<script>alert("Hizmet Güncellendi.")</script><meta http-equiv="refresh" content="0; url=kategoriler.php">';
+        } else {
+            echo '<script>alert("Hata Oluştu")</script><meta http-equiv="refresh" content="0; url=kategoriler.php">';
+        }
+    } else {
+        $guncelle = $db->prepare('update kategoriler set tur=?,durum=?,aciklama=?, where id=?');
+        $guncelle->execute(array($_POST['turUp'], $_POST['durumUp'], $_POST['aciklamaUp'], $id));
+        if ($guncelle->rowCount()) {
+            echo '<script>alert("Hizmet Güncellendi.")</script><meta http-equiv="refresh" content="0; url=kategoriler.php">';
+        } else {
+            echo '<script>alert("Hata Oluştu")</script><meta http-equiv="refresh" content="0; url=kategoriler.php">';
+        }
+    }
+}
+
+?>
+
+
+<!-- Update Modal Module End -->
+
+
+
+
+
 <?php require_once('footer.php'); ?>

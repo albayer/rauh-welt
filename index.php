61d04a9125623a78d4c631e658e9343b247b3fea
albayer <alperenbayer@gmail.com>
2025-08-16 15:58:36 +0300

    add
    index.php düzenlendi. Eklemeler yapılabilir.

--- index.php
+++ index.php
@@ -14,9 +14,12 @@ require_once('./header.php');
     <?php
     require_once('./banner.php');
     ?>
+    <!-- Models Section Start -->
     <section id="models" class="my-4">
         <div class="container">
+            <h3 class="fs-3">Dünyaca Ünlü Motosiklet Markalarından Birkaçını Sizler İçin Seçtik</h3>
             <div class="row">
+                <!-- Marka Listeleme Module Start -->
                 <?php
                 $model = $db->prepare('select * from marka_model order by id desc limit 9');
                 $model->execute();
@@ -42,20 +45,60 @@ require_once('./header.php');
                     }
                 }
                 ?>
+                <!-- Marka Listeleme Module End -->
+            </div>
+            <div style="display:flex; justify-content:center;">
+                <button class="btn btn-success" style="display: flex; flex-direction: row; justify-content: center; margin-top: 15px;">
+                    <a href="modeller.php" class="text-white">Tüm Modeller İçin Tıklayınız</a>
+                </button>
             </div>
         </div>
     </section>
-
-
-
-
-
-
-
-
-
-
-
+    <!-- Models Section End -->
+    <!-- CTA Section Start -->
+    <section id="cta" style="background-color: #000;" class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <a href="iletisim.php" class="text-white fs-4">Yol Yardım ve Servis Ağımızla İlgili Hizmet Almak İçin Bizimle İletişime Geçebilirsiniz.</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- CTA Section End -->
+    <!-- Choise Section Start -->
+    <section id="choise" class="py-5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6" style="display: flex; align-items:center; flex-direction:column; justify-content:center;">
+                    <b class="fs-3">SİZİN MOTOSİKLETİNİZ.SİZİN KURALLARINIZ.</b><br>
+                    <b>Motosikletinizi, vizyonunuzu hayata geçiren parçalar ve aksesuarlarla özelleştirin.</b>
+                    <a href="aksesuarlar.php" class="pt-4"><button class="btn btn-primary">Aksesuarlara Göz At</button></a>
+                </div>
+                <div class="col-md-6">
+                    <img src="./assets/img/choise.webp" alt="Aksesuar Reklamı İçin Kullanılan Görsel" class="img-fluid" style="margin-left: 12px;">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Choise Section End -->
+    <!-- Email Section Start -->
+    <section id="email" style="background-color: #000;" class="py-5 mb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 my-auto">
+                    <span class="fs-5 text-white">Fırsatlardan Haberdar Olmak İçin E-Bülten'e Üye Olun</span>
+                </div>
+                <div class="col-md-6">
+                    <form method="get" style="display:flex; flex-direction:row; gap:10px;">
+                        <input type="text" name="email" placeholder="Email Adresiniz" class="form-control">
+                        <input type="submit" value="Üye Ol" name="uyeol" class="btn btn-success w-25">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Email Section End -->
     <?php
     require_once('./footer.php');
     ?>
